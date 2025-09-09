0b9ee2bdcde5c87fc647f5c3ce7812ef0a1a4c50
Izn Ur Rehman Khan <besttime444444@gmail.com>
2025-09-09 15:40:47 +0500

    user can delete product & slightly change in the UI of product list

--- lib/features/listing/presentation/screens/all_listings_screen.dart
+++ lib/features/listing/presentation/screens/all_listings_screen.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:loopify/app.dart';
 import 'package:loopify/core/theme/colors.dart';
+import 'package:loopify/core/utils/formaters.dart';
 import 'package:loopify/features/listing/data/model/product/product.dart';
 import 'package:loopify/features/listing/presentation/screens/create_listing_screen.dart';
 
@@ -12,7 +15,6 @@ class AllListingsScreen extends StatefulWidget {
 }
 
 class _AllListingsScreenState extends State<AllListingsScreen> {
-
   bool _isLoading = false;
   late List<Product> products;
 
@@ -55,16 +57,52 @@ class _AllListingsScreenState extends State<AllListingsScreen> {
                           ),
                         );
                       },
-                      title: Text(products[index].title),
-                      shape: RoundedRectangleBorder(
-                        side: BorderSide(
-                          color: AppColors.disabledButtonForegroungColor,
-                        ),
-                        borderRadius: BorderRadiusGeometry.circular(4),
+                      leading: Image.file(
+                        File(products[index].imagePaths[0]),
+                        width: 48,
+                        height: 48,
+                        fit: BoxFit.cover,
                       ),
+                      title: Text(
+                        products[index].title,
+                        overflow: TextOverflow.fade,
+                        style: Theme.of(
+                          context,
+                        ).textTheme.bodyMedium?.copyWith(),
+                      ),
+                      subtitle: Text(
+                        priceFormater(products[index].price),
+                        style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                          color: AppColors.primary,
+                          height: 1,
+                        ),
+                      ),
+                      trailing: IconButton(
+                        onPressed: () {
+                          productLocalRepository.deleteProductById(
+                            products[index].id!,
+                          );
+                        },
+                        icon: Icon(
+                          Icons.delete_outline_rounded,
+                          color: AppColors.error,
+                        ),
+                      ),
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 0,
+                      ),
+                      isThreeLine: true,
+                      visualDensity: VisualDensity.compact,
+                      // shape: RoundedRectangleBorder(
+                      //   side: BorderSide(
+                      //     color: AppColors.disabledButtonForegroungColor,
+                      //   ),
+                      //   borderRadius: BorderRadiusGeometry.circular(4),
+                      // ),
                     ),
                   ),
-                  separatorBuilder: (_, _) => const SizedBox(height: 20),
+                  separatorBuilder: (_, _) => const SizedBox(height: 5),
                 ),
               ),
       ),
